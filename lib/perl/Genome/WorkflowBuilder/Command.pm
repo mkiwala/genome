1526bfbd42180c7b5b6a2b04c60498bde3c706ae
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-09-30 12:05:19 -0500

    dummy commit -- work in progress

--- lib/perl/Genome/WorkflowBuilder/Command.pm
+++ lib/perl/Genome/WorkflowBuilder/Command.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Genome;
 use Cwd qw();
+use Genome::Utility::LSFResourceParser qw(parse_lsf_params);
 
 
 class Genome::WorkflowBuilder::Command {
@@ -90,22 +91,28 @@ sub _get_ptero_execute_method {
 
     my $self = shift;
     my $log_dir = shift;
+    my $ptero_lsf_parameters = $self->_get_ptero_lsf_parameters();
+    $ptero_lsf_parameters->{command} = sprintf(
+        'genome ptero wrapper --command-class %s '
+        .'--method execute --log-directory %s',
+        $self->command, $log_dir);
+    $ptero_lsf_parameters->{environment} = $self->_get_sanitized_env();
+    $ptero_lsf_parameters->{user} = Genome::Sys->username;
+    $ptero_lsf_parameters->{cwd} = Cwd::getcwd;
+
     return Ptero::Builder::Job->new(
         name => 'execute',
-        # XXX This should use the LSF service, or be configuration based
-        service_url => Genome::Config::get('ptero_shell_command_service_url'),
-        parameters => {
-            commandLine => [
-                'genome', 'ptero', 'wrapper',
-                '--command-class', $self->command,
-                '--method', 'execute',
-                '--log-directory', $log_dir,
-            ],
-            environment => $self->_get_sanitized_env(),
-            user => Genome::Sys->username,
-            workingDirectory => Cwd::getcwd,
-        },
-    );
+        service_url => Genome::Config::get('ptero_lsf_service_url'),
+        parameters => $ptero_lsf_parameters);
+}
+
+sub _get_ptero_lsf_parameters {
+    my $self = shift;
+    my $command_class = $self->command;
+    return Genome::Utility::LSFResourceParser::parse_lsf_params(
+        $command_class->can('lsf_resource')
+            ? $command_class->lsf_resource
+            : '');
 }
 
 
